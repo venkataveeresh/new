7816cc1972501ab427e863b0844c6160b0632907
venkataveeresh <veeresh1716@gmail.com>
2025-06-25 00:39:33 +0530

    new code upadted for newcode.sh

---
+++ newcode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+<<intro
+Author : veeresh
+Disc   : how to print numbers from 0 to 10 using for loop.
+=========================================================
+intro
+
+for i in {1..10};do 
+	echo "$i"
+done
+
