77e3b6c45566255d2f24a06a7a76bbd48ef49b6f
venkataveeresh <veeresh1716@gmail.com>
2025-06-25 00:02:30 +0530

    updated code in veera.sh

---
+++ veera.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<<intro
+Author : veeresh
+Disc   : write a script to print current year,month, date, time.
+================================================================
+intro
+current_year=$(date +%Y)
+echo "year is $current_year"
+current_month=$(date +%m)
+echo "Month is $current_month"
+current_date=$(date +%d)
+echo "date is $current_date"
+current_time=$(date +%T)
+echo "Time is $current_time"
